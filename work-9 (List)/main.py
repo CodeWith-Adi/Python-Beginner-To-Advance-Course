43349729365011ad1d926123d19f3e0ce117a6dd
CodeWith-Adi <codewithadi28312@gamil.com>
2025-03-19 12:03:28 +0530

    Init

---
+++ work-9 (List)/main.py
@@ -0,0 +1,49 @@
+# l = ["Adi", 1, 5, 4]
+# print(l)
+
+# indexing and slicing in lists
+# mutablity
+# l[1] = 8
+# print(l)
+#indexing
+# print(l[2])
+# print(l[-1])
+# print(len(l)) for checing the length of the list
+#slicing in list
+# print(l[1:])
+# List Methods
+# 1. append():
+# my_list = [1, 2, 3]
+# my_list.append(4)
+# print(my_list)
+
+# 2. extend():
+# my_list.extend([5, 6, 7])
+
+# 3. insert():
+# my_list.insert(3, 4)
+
+# 4. remove():
+# my_list.remove(7)
+
+# 5. pop():
+# print(my_list.pop(5))
+
+# 6. clear():
+# my_list.clear()
+# print(my_list)
+
+# 7. sort():
+my_list = [1, 3, 3, 3, 3, 2, 5, 4]
+my_list.sort()
+print(my_list)
+
+# 8. reverse():
+my_list.reverse()
+print(my_list)
+
+# 9. index
+print(my_list.index(2))
+
+# 10. count():
+print(my_list.count(3))
