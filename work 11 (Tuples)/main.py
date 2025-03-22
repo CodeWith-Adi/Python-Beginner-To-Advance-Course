99a235e17052708f1ecfe20fc1991a9a5a55107e
CodeWith-Adi <codewithadi28312@gamil.com>
2025-03-22 10:35:19 +0530

    Init

---
+++ work 11 (Tuples)/main.py
@@ -0,0 +1,42 @@
+my_tuple = 1, 2, 2, 3
+
+# Ordered and allow duplicates
+# print(my_tuple)
+
+# Immutable
+# my_tuple[0] = 4 # TypeError
+
+# Common Operations
+
+# Indexing
+
+# print(my_tuple[0])
+
+# Slicing
+# print(my_tuple[1:3])
+
+# Length
+# print(len(my_tuple))
+
+# Concatenation
+# tuple1 = (1, 2)
+# tuple2 = (3, 4)
+
+# print(tuple1 + tuple2)
+
+# Important Methods for Tuples
+
+# count
+# print(my_tuple.count(2))
+
+# index
+# print(my_tuple.index(2))
+
+# max
+# print(max(my_tuple))
+
+# min
+# print(min(my_tuple))
+
+# sum
+print(sum(my_tuple))
