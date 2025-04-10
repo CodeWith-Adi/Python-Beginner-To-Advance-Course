a511f2906de0dbad46eb3e7dd489dc4f75444e9f
CodeWith-Adi <codewithadi28312@gamil.com>
2025-04-10 13:42:40 +0530

    Init

---
+++ work 15 (Differnce beetween List, Tuples and Sets)/README.md
@@ -0,0 +1,8 @@
+# Difference Between List, Tuples and Sets. 
+List | Tuples | Sets |
+:----------:|:-----------:|:-----------:|
+Ordered | Orderd | Unordered |
+Mutable | Immutable | Mutable |
+Indexed | Indexed | Not Indexed |
+Duplicates allowed | Duplicates allowed |No duplicates allowed|
+Defined using square brackts `[]`| Defined using parentheses `()` | Difined using curly brackets `{}` |
---
+++ work 15 (Differnce beetween List, Tuples and Sets)/main.py
@@ -0,0 +1,41 @@
+# Orderation
+# my_list = [1, 3, 4, 2, 5]
+# print(my_list)
+# my_tuple = (1, 3, 4, 2, 5)
+# print(my_tuple)
+# my_sets = {1, 3, 4, 2, 5}
+# print(my_sets)
+
+# Mutablity
+# my_list = [1, 3, 4, 2, 5]
+# print(my_list)
+
+# my_list[0] = 10
+# print(my_list)
+
+# my_tuple = (1, 3, 4, 2, 5)
+# my_tuple[0] = 10
+
+# my_sets = {1, 3, 4, 2, 5}
+# my_sets[0] = 10
+
+# indexing
+# my_list = [1, 3, 4, 2, 5]
+# print(my_list[0])
+
+# my_tuple = (1, 3, 4, 2, 5)
+# print(my_tuple[0])
+
+# my_sets = {2, 3, 4, 1, 5}
+# print(my_sets[0])
+
+# Duplicates
+
+my_list = [1, 1, 1, 1, 1]
+print(my_list)
+
+my_tuple = (1, 1, 1, 1, 1)
+print(my_tuple)
+
+my_set = {1, 1, 1, 1, 1, True}
+print(my_set)
