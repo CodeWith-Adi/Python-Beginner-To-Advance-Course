739240170ad073eb1f3cb48fec18094734e37683
CodeWith-Adi <codewithadi28312@gamil.com>
2025-04-14 12:35:43 +0530

    Init

---
+++ work 16 (Complex numbers)/main.py
@@ -0,0 +1,14 @@
+z1 = 3 + 5j
+# print(z1)
+# print(type(z1))
+
+z2 = complex(2, 6)
+# print(z2)
+# print(type(z2))
+
+# print(z1.real)
+
+print(z1 + z2)
+print(z1 - z2)
+print(z1 * z2)
+print(z1 / z2)
