8c8047f49c26423149f4f87a5a76fd591459fd5e
CodeWith-Adi <codewithadi28312@gamil.com>
2025-06-19 16:29:44 +0530

    Init

---
+++ work 28 (functions)/main.py
@@ -0,0 +1,18 @@
+# normal function
+def my_function():
+    print("Hello!")
+
+# function with parameters
+def add(a, b):
+    print(a + b)
+
+# use of return
+def multiply(a, b):
+    return a*b
+
+# calling functions
+my_function()
+add(5, 2)
+result = multiply(5, 2)
+print(result)
+add(result, 2)
